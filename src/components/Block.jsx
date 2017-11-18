faf444f0ea7df8c608e806b1d3e8746491ed5ee4
Justin Mak <jkmak02@gmail.com>
2017-11-18 11:23:19 +0900

    Change Block to PureComponent

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,25 +1,27 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 const random255 = () => Math.floor(Math.random() * 256);
 const randomColor = () => `rgb(${random255()}, ${random255()}, ${random255()})`;
 
-export default function Block({ className, label }) {
-  return (
-    <div className={className} style={{backgroundColor: randomColor()}}>
-      {label}
-    </div>
-  );
+export default class  Block extends PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    label: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  }
+
+  static defaultProps = {
+    className: '',
+    label: '#',
+  }
+
+  render() {
+    const { className, label } = this.props;
+
+    return (
+      <div className={className} style={{backgroundColor: randomColor()}}>
+        {label}
+      </div>
+    );
+  }
 }
-
-Block.propTypes = {
-  className: PropTypes.string,
-  label: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-};
-
-Block.defaultProps = {
-  className: '',
-  label: '#',
-};
-
-Block.displayName = 'Block';
